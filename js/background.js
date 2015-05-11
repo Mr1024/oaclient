19be63cfbbcac39e7f0a8c773b803c880cf413b3
zhangyizhong <zhangyizhong@news.cn>
2015-05-11 18:02:18 +0800

    update

--- js/background.js
+++ js/background.js
@@ -1,75 +1,193 @@
- var i = 1;
- var socket = io.connect('http://localhost:8080');
- var lastId;
- socket.on('connect', function() {
-     lastId = 1;
-     socket.emit("serverstatus", lastId);
-     socket.on("clientstatus", function(data) {
-         console.log("lastId" + data);
-     });
- });
- chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-     if (message.type == 'verify') {
-         console.log("verify");
-         sendResponse("ok");
-         showNotification({
-             type: "公告",
-             text: "关于胡鑫同志任前公示的公告",
-             asidetxt: "2015-05-08 16:18 彭谆 ",
-             priority: 2
-         }, function(notificationId) {
+var BGTool = {
+    storageget: function(str) {
+        var obj = localStorage[str];
+        if (obj) {
+            return JSON.parse(obj);
+        } else {
+            return {};
+        }
+    },
+    storageset: function(key, value) {
+        if (typeof value == "object") {
+            value = JSON.stringify(value);
+        }
+        localStorage[key] = value;
+    },
+    sendMessage: function(data, callback) {
+        chrome.runtime.sendMessage(data,
+            function(data) {
+                callback(data);
+            });
+    }
+};
 
-         });
-         $.get('http://www.baidu.com', function(data) {
+var i = 1;
+var socket = io.connect('http://localhost:8080');
+var lastId;
+var currUserObj;
+var userStatus;
+socket.on('connect', function() {
+    var userObj = BGTool.storageget('user');
+    if (userObj) {
+        socket.emit("getInfoReq", userObj);
+        socket.on('getInfoRes', function(data) {
+            if (data.password != undefined) {
+                currUserObj = data;
+            } else {
+                currUserObj = {};
+            }
+        });
+    } else {
+        currUserObj = {};
+    }
+    lastId = 1;
+    socket.emit("serverstatus", lastId);
+    socket.on("clientstatus", function(data) {
+        console.log("lastId" + data);
+    });
+});
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    userStatus = $.Deferred();
+    if (message.type == 'verify') {
+        currUserObj = message.data;
+        login(currUserObj);
+        /*userStatus.done(function(status) {
+            console.log("1"+status);
+            sendResponse(1);
+        }).fail(function(status) {
+            if (status == 2) {
+                sendResponse(2);
+            } else {
+                sendResponse(3);
+            }
+        });*/
+    } else if (message.type == 'getinfo') {
+        sendResponse(currUserObj);
+    } else if (message.type == 'logout') {
+        currUserObj = {};
+        localStorage.removeItem('user');
+        sendResponse("ok");
+    } else {
+        sendResponse("ok");
+        showNotification({
+            type: "公告",
+            text: "关于胡鑫同志任前公示的公告",
+            asidetxt: "2015-05-08 16:18 彭谆 ",
+            priority: 2
+        }, function(notificationId) {
+            login();
+        });
+    }
+});
+/* $.get('http://www.baidu.com', function(data) {
 
-         });
-     } else if (message.type == 'getinfo') {
+ });*/
+var loginStatus = null;
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+    //console.log(details);
+    /*for (var i = 0, headerLen = details.requestHeaders.length; i < headerLen; ++i) {
+        if (details.requestHeaders[i].name == 'User-Agent') {
+            details.requestHeaders.splice(i, 1);
+            break;
+        }
+    }*/
+    loginStatus = null;
+    details.requestHeaders.push({
+        name: "Origin",
+        "value": "http://172.18.1.48"
+    }, {
+        name: "Referer",
+        value: "http://172.18.1.48/seeyon/index.jsp"
+    });
+    return {
+        requestHeaders: details.requestHeaders
+    };
+}, {
+    urls: [
+        "http://172.18.1.48/seeyon/login/proxy"
+    ]
+}, [
+    "blocking",
+    "requestHeaders"
+]);
+chrome.webRequest.onHeadersReceived.addListener(function(details) {
+    if (details.url == 'http://172.18.1.48/seeyon/login/proxy') {
+        for (var i = 0, headerLen = details.responseHeaders.length; i < headerLen; ++i) {
+            if (details.responseHeaders[i].name == 'LoginError') {
+                var LoginErrorV = details.responseHeaders[i].value;
+                loginStatus = {
+                    "LoginError": LoginErrorV
+                };
+                switch (LoginErrorV) {
+                    case '7':
+                        setTimeout(function() {
+                            login()
+                        }, Math.random() * 10000);
+                        break;
+                    case '2':
+                        BGTool.sendMessage({
+                            type: "verify",
+                            data: 2
+                        });
+                        break;
+                    case '1':
+                        BGTool.sendMessage({
+                            type: "verify",
+                            data: 3
+                        });
+                        break;
+                }
+            } else if (details.responseHeaders[i].name == 'LoginOK') {
+                BGTool.sendMessage({
+                    type: "verify",
+                    data: 1
+                });
+                break;
+            }
+        }
+    }
+}, {
+    urls: [
+        "<all_urls>"
+    ]
+}, [
+    "blocking", "responseHeaders"
+]);
 
-     }
- });
- $.get('http://www.baidu.com', function(data) {
+function login(userObj) {
+    $.post('http://172.18.1.48/seeyon/login/proxy', {
+            "UserAgentFrom": "pc",
+            "login.timezone": "",
+            "login.username": "zhangyizhong",
+            "login.password": "zhangyizhong",
+            "authorization": "",
+            "dogSessionId": ""
+        },
+        function(data) {
 
- });
- chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
-     console.log(details);
-     for (var i = 0, headerLen = details.requestHeaders.length; i < headerLen; ++i) {
-         if (details.requestHeaders[i].name == 'User-Agent') {
-             details.requestHeaders.splice(i, 1);
-             break;
-         }
-     }
-     return {
-         requestHeaders: details.requestHeaders
-     };
- }, {
-     urls: [
-         "<all_urls>"
-     ]
- }, [
-     "blocking",
-     "requestHeaders"
- ]);
+        });
+}
 
- function showNotification(txtObj, callback) {
-     console.log(txtObj)
-     if (txtObj.priority == 'undefined') {
-         txtObj.priority = 0;
-     }
-     chrome.notifications.create({
-         type: "basic",
-         title: txtObj.type,
-         message: txtObj.text,
-         iconUrl: "images/icon-128.png",
-         contextMessage: txtObj.asidetxt,
-         buttons: [{
-             title: "查看"
-         }],
-         priority: txtObj.priority,
-         isClickable: true
-     }, function(notificationId) {
-         callback(notificationId);
-     });
- }
- chrome.notifications.onButtonClicked.addListener(function(id) {
-     console.log(id);
- });
+function showNotification(txtObj, callback) {
+    console.log(txtObj)
+    if (txtObj.priority == 'undefined') {
+        txtObj.priority = 0;
+    }
+    chrome.notifications.create({
+        type: "basic",
+        title: txtObj.type,
+        message: txtObj.text,
+        iconUrl: "images/icon-128.png",
+        contextMessage: txtObj.asidetxt,
+        buttons: [{
+            title: "查看"
+        }],
+        priority: txtObj.priority,
+        isClickable: true
+    }, function(notificationId) {
+        callback(notificationId);
+    });
+}
+chrome.notifications.onButtonClicked.addListener(function(id) {
+    console.log(id);
+});
--- js/popup.js
+++ js/popup.js
@@ -2,8 +2,11 @@ var OATool = {
     showTip: function(txt) {
         $(".msgremindbox").text(txt).slideDown();
     },
-    closeTip: function() {
-        $(".msgremindbox").slideUp();
+    closeTip: function(time) {
+        var time = time || 0;
+        setTimeout(function() {
+            $(".msgremindbox").slideUp();
+        }, time);
     },
     loginboxshow: function() {
         $(".loginbox").css({
@@ -17,143 +20,193 @@ var OATool = {
             opacity: 0
         });
     },
-    sendMsg: function(data, callback) {
+    sendMessage: function(data, callback) {
         chrome.runtime.sendMessage(data,
             function(data) {
                 callback(data);
             });
     },
-    storageset: function(obj, callback) {
-        chrome.storage.sync.set(obj,
-            function() {
-                callback();
-            });
-    },
-    storageget: function(str, callback) {
-        chrome.storage.sync.get(str, function(result) {
-            callback(result[str]);
-        });
+    onMessage: function(callback) {
+        chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+            callback(message, sender, sendResponse);
+        })
     }
 }
-
-
-var i = 1;
-var socket = io.connect('http://localhost:8080');
-OATool.storageset({
-    "user": {
-        "name": "zhangyizhong",
-        "status": 2,
-        "lastMsgId": "123"
-    }
-}, function() {
-    console.log("t1");
-});
-OATool.storageget("user", function(data) {
-    var userObj = data;
-    if (userObj) {
-        if (userObj.status == 1) {
-            $(".username").text(userObj.name);
-            $(".usernamebox input").val(userObj.name);
-             OATool.sendMsg({
-                "type": "getinfo",
-                "data": {
-                    "name": userObj.name
-                }
-            },function(data){
-            	console.log(data);
-            });
-        } else if (userObj.status == 2) {
-            $(".username").text(userObj.name);
-            $(".usernamebox input").val(userObj.name);
-            OATool.sendMsg({
-                "type": "verify",
-                "data": {
-                    "name": userObj.name
-                }
-            },function(data){
-            	console.log(data);
-            });
-            OATool.showTip("账户正在验证，请稍后");
-        } else {
-            OATool.loginboxshow();
-        }
+var userObj;
+OATool.sendMessage({
+    type: "getinfo"
+}, function(data) {
+    if (data.name) {
+        $(".usernamebox input").val(data.name);
+        $(".passwordbox input").val(data.password);
+        $(".username").text(data.name);
+        userObj = data;
     } else {
         OATool.loginboxshow();
     }
+    init();
 });
-/*var user = localStorage.user;
-var userObj
-if (user) {
-    userObj = JSON.parse(user);
-    $(".username").text(userObj.name);
-    $(".usernamebox input").val(userObj.name);
-    $(".passwordbox input").val(userObj.password)
-} else {
-    $(".loginbox").css({
-        top: "0px",
-        opacity: 1
-    });
-}*/
-$(".settingbtn").on("click", function() {
-    $(".loginbox").css({
-        top: "0px",
-        opacity: 1
-    });
+OATool.onMessage(function(message, sender, sendResponse) {
+    console.log(message);
+    if (message.type = "verify") {
+        if (message.data == 1) {
+            OATool.showTip("账号验证通过");
+            OATool.closeTip(5000);
+        } else if (message.data == 2) {
+            OATool.showTip("密码错误，谨慎输入");
+        } else {
+            OATool.showTip("用户名错误");
+        }
+    }
 });
-$(".loginbox .canclebtn").on("click", function() {
-    $(".loginbox").css({
-        top: "600px",
-        opacity: 0
+
+function init() {
+    var i = 1;
+    var socket = io.connect('http://localhost:8080');
+    $(".settingbtn").on("click", function() {
+        $(".loginbox").css({
+            top: "0px",
+            opacity: 1
+        });
     });
-});
-$(".loginbox .sigininbtn").on("click", function() {
-    var username = $.trim($(".usernamebox input").val());
-    var password = $.trim($(".passwordbox input").val());
-    if (username.length == 0) {
-        $(".usernamebox input").css("border-color", "red").focus();
-    } else if (password.length == 0) {
-        $(".passwordbox input").css("border-color", "red").focus();
-    } else {
+    //自动登录
+    $(".powerbtn").on("click", function() {
+        if (!userObj) {
+            OATool.loginboxshow();
+        } else {
+            OATool.showTip("自动登录中，请稍后");
+            OATool.sendMessage({
+                type: "verify",
+                data: userObj
+            }, function(data) {
+                if (data == 1) {
+                    OATool.showTip("登录成功");
+                    window.open('http://172.18.1.48/seeyon/main.do?method=index', 'suboa');
+                    OATool.closeTip(5000);
+                } else if (data == 2) {
+                    OATool.showTip("密码错误，谨慎输入");
+                } else {
+                    OATool.showTip("用户名错误");
+                }
+
+            });
+        }
+    });
+    //退出账户
+    $(".signoutbtn").on("click", function() {
+        $.get('http://172.18.1.48/seeyon/login/logout', function(data) {
+            console.log(data);
+            OATool.sendMessage({
+                type: "logout"
+            }, function(data) {
+                if (data == "ok") {
+                    $(".usernamebox input").val("");
+                    $(".passwordbox input").val("");
+                    OATool.loginboxshow();
+                }
+            })
+        });
+    });
+    $(".loginbox .canclebtn").on("click", function() {
+        if (!userObj) {
+            return
+        }
         $(".loginbox").css({
             top: "600px",
             opacity: 0
         });
-        $(".username").text(username);
-        if (userObj) {
-            if (username != userObj.name || password != userObj.password) {
+    });
+    $(".passwordbox input").keydown(function(e) {
+        if (e.keyCode == 13) {
+            $(".loginbox .sigininbtn").trigger("click");
+        }
+    });
+    $(".loginbox .sigininbtn").on("click", function() {
+        var username = $.trim($(".usernamebox input").val());
+        var password = $.trim($(".passwordbox input").val());
+        if (username.length == 0) {
+            $(".usernamebox input").css("border-color", "red").focus();
+        } else if (password.length == 0) {
+            $(".passwordbox input").css("border-color", "red").focus();
+        } else {
+            $(".loginbox").css({
+                top: "600px",
+                opacity: 0
+            });
+            $(".username").text(username);
+            if (userObj) {
+                console.log(password);
+                if (username != userObj.name) {
+                    OATool.showTip("账户正在验证，请稍后");
+                    OATool.sendMessage({
+                        type: "verify",
+                        data: {
+                            username: username,
+                            password: password
+                        }
+                    }, function(data) {
+                        if (data == 1) {
+                            OATool.showTip("账号验证通过");
+                            OATool.closeTip(5000);
+                        } else if (data == 2) {
+                            OATool.showTip("密码错误，谨慎输入");
+                        } else {
+                            OATool.showTip("用户名错误");
+                        }
+
+                    });
+                } else {
+                    if (password == userObj.password) {
+                        OATool.loginboxclose();
+                    } else {
+                        $(".passwordbox input").css("border-color", "red").focus();
+                    }
+                }
+            } else {
                 OATool.showTip("账户正在验证，请稍后");
                 OATool.sendMessage({
-                    type: "verifypw",
+                    type: "verify",
                     data: {
                         username: username,
                         password: password
                     }
                 }, function(data) {
-
+                    console.log(data);
+                    if (data == 1) {
+                        OATool.showTip("账号验证通过");
+                        userObj = {
+                            name: username,
+                            password: password
+                        };
+                        OATool.closeTip(5000);
+                    } else if (data == 2) {
+                        OATool.showTip("密码错误，谨慎输入");
+                    } else {
+                        OATool.showTip("用户名错误");
+                    }
                 });
-            } else {
-                OATool.showTip("账户正在验证，请稍后");
             }
         }
-    }
-});
-$(".usernamebox input,.passwordbox input").keyup(function() {
-    if ($.trim($(this).val()).length > 0) {
-        $(this).css("border-color", "#D1D1D1");
-    }
-});
-socket.on('connect', function() {
-    socket.on('message', function(message) {
-        $("ul").append("<li>" + message + "</li>");
     });
-    socket.on('disconnect', function() {
-        console.log("close");
+    $(".usernamebox input,.passwordbox input").keyup(function() {
+        if ($.trim($(this).val()).length > 0) {
+            $(this).css("border-color", "#D1D1D1");
+        }
     });
-    socket.on("test", function(data) {
-        $("ul").append("<li>test" + data + "</li>");
-    })
-    setInterval(function() {
-        socket.send(i++);
-    }, 1000);
+    socket.on('connect', function() {
+        socket.on('message', function(message) {
+            $("ul").append("<li>" + message + "</li>");
+        });
+        socket.on('disconnect', function() {
+            console.log("close");
+        });
+        socket.on("test", function(data) {
+            $("ul").append("<li>test" + data + "</li>");
+        })
+        setInterval(function() {
+            socket.send(i++);
+        }, 1000);
 
-});
+    });
+
+}
